874872da0067144f1bc08c2c6ae2330919854f88
Paula <paltolaguirre@finnegans.com.ar>
2020-01-17 12:03:42 -0300

    [feature/paltolaguirre-svc84]
    Nuevas formulas

--- calculosAutomaticos/formulas.go
+++ calculosAutomaticos/formulas.go
@@ -293,8 +293,211 @@ func getGananciaNeta(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) fl
 	return gananciaNeta
 }
 
-func getDetalleCargoFamiliar(liquidacion *structLiquidacion.Liquidacion, columnaDetalleCargoFamiliar string, db *gorm.DB) float64 {
-	var importeTotal, valorbeneficio float64
+func getConyuge(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	importeTotal := getDetalleCargoFamiliar(liquidacion, "conyugeid", "valorfijoconyuge", 1, db)
+	return importeTotal
+}
+
+func getHijos(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	var importeTotal float64 = 0
+	var detallescargofamiliarsiradig []structSiradig.Detallecargofamiliarsiradig
+
+	valorfijoMNI := getValorFijoImpuestoGanancia(liquidacion, "deduccionespersonales", "valorfijomni", db)
+	sql := "SELECT * FROM detallecargofamiliar WHERE hijoid NOT NULL AND estaacargo = true AND montoanual < " + strconv.FormatFloat(valorfijoMNI, 'f', 5, 64)
+	db.Raw(sql).Scan(&detallescargofamiliarsiradig)
+
+	for i := 0; i < len(detallescargofamiliarsiradig); i++ {
+		porcentaje := detallescargofamiliarsiradig[i].Porcentaje
+		importeTotal = importeTotal + getDetalleCargoFamiliar(liquidacion, "hijoid", "valorfijohijo", *porcentaje, db)
+	}
+
+	return importeTotal
+}
+
+func getMinimoNoImponible(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	valorfijoMNI := getValorFijoImpuestoGanancia(liquidacion, "deduccionespersonales", "valorfijomni", db)
+	mesperiodoliquidacion := getMes(&liquidacion.Fechaperiodoliquidacion)
+	importeTotal := (valorfijoMNI / 12) * float64(mesperiodoliquidacion)
+	return importeTotal
+}
+
+func getDeduccionEspecial(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	valorfijoMNI := getValorFijoImpuestoGanancia(liquidacion, "deduccionespersonales", "valorfijoddei", db)
+	mesperiodoliquidacion := getMes(&liquidacion.Fechaperiodoliquidacion)
+	importeTotal := (valorfijoMNI / 12) * float64(mesperiodoliquidacion)
+	return importeTotal
+}
+
+func getSubtotalDeduccionesPersonales(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	var arraySubtotalDeduccionesPersonales []float64
+	var subTotalDeduccionesPersonales float64
+
+	arraySubtotalDeduccionesPersonales = append(arraySubtotalDeduccionesPersonales, getConyuge(liquidacion, db))
+	arraySubtotalDeduccionesPersonales = append(arraySubtotalDeduccionesPersonales, getHijos(liquidacion, db))
+	arraySubtotalDeduccionesPersonales = append(arraySubtotalDeduccionesPersonales, getMinimoNoImponible(liquidacion, db))
+	arraySubtotalDeduccionesPersonales = append(arraySubtotalDeduccionesPersonales, getDeduccionEspecial(liquidacion, db))
+
+	subTotalDeduccionesPersonales = Sum(arraySubtotalDeduccionesPersonales)
+	return subTotalDeduccionesPersonales
+}
+
+func getDeduccionesAComputar(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	importeTotal := getSubtotalDeduccionesPersonales(liquidacion, db)
+	return importeTotal
+}
+
+func getGananciaNetaAcumSujetaAImp(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	//Ver con Rodri como manejar este caso
+	return 0.6
+}
+
+func getDeduccionesPersonales(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	importeTotal := getDeduccionesAComputar(liquidacion, db)
+	return importeTotal * -1
+}
+
+func getBaseImponible(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	var arrayBaseImponible []float64
+	var importeTotal float64
+
+	arrayBaseImponible = append(arrayBaseImponible, getGananciaNetaAcumSujetaAImp(liquidacion, db))
+	arrayBaseImponible = append(arrayBaseImponible, getDeduccionesPersonales(liquidacion, db))
+
+	importeTotal = Sum(arrayBaseImponible)
+	return importeTotal
+}
+
+func getTotalGananciaNetaImponibleAcumuladaSinHorasExtras(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+
+	cuotaSindical := getCuotaSindical(liquidacion, db)
+	obraSocial := getAportesObraSocial(liquidacion, db)
+	aportesJubilatorios := getAportesJubilatoriosRetirosPensionesOSubsidios(liquidacion, db)
+	remunerativosMenosDescuentos := obtenerRemunerativosMenosDescuentos(liquidacion)
+	cuotaSindicalOtros := getCuotaSindicalOtrosEmpleos(liquidacion, db)
+	obraSocialOtros := getAportesObraSocialOtrosEmpleos(liquidacion, db)
+	aportesJubilatoriosOtros := getAportesJubilatoriosRetirosPensionesOSubsidiosOtrosEmpleos(liquidacion, db)
+	remunerativosOtros := obtenerRemunerativosOtros(liquidacion, db)
+
+	var uno float64 = 1
+	porcentaje := uno - (cuotaSindical/remunerativosMenosDescuentos + obraSocial/remunerativosMenosDescuentos + aportesJubilatorios/remunerativosMenosDescuentos)
+	porcentajeOtrosEmp := uno - (cuotaSindicalOtros/remunerativosOtros + obraSocialOtros/remunerativosOtros + aportesJubilatoriosOtros/remunerativosOtros)
+
+	importeTotal := getBaseImponible(liquidacion, db) - (getHorasExtrasGravadas(liquidacion, db)*porcentaje + getHorasExtrasGravadasOtrosEmpleos(liquidacion, db)*porcentajeOtrosEmp)
+	return importeTotal
+}
+
+func obtenerRemunerativosMenosDescuentos(liquidacion *structLiquidacion.Liquidacion) float64 {
+	var totalRemunerativos, totalDescuentos float64
+	for i := 0; i < len(liquidacion.Liquidacionitems); i++ {
+		liquidacionitem := liquidacion.Liquidacionitems[i]
+		tipoconcepto := *liquidacionitem.Concepto.Tipoconceptoid
+		importeconcepto := *liquidacionitem.Importeunitario
+		if tipoconcepto == -1 {
+			totalRemunerativos = totalRemunerativos + importeconcepto
+		}
+		if tipoconcepto == -3 {
+			totalDescuentos = totalDescuentos + importeconcepto
+		}
+
+	}
+	return totalRemunerativos - totalDescuentos
+}
+
+func obtenerRemunerativosOtros(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	var arrayRemunerativosOtros []float64
+	var totalRemunerativosOtros float64
+
+	arrayRemunerativosOtros = append(arrayRemunerativosOtros, getRemuneracionBrutaOtrosEmpleos(liquidacion, db))
+	arrayRemunerativosOtros = append(arrayRemunerativosOtros, getSacPrimerCuotaOtrosEmpleos(liquidacion, db))
+	arrayRemunerativosOtros = append(arrayRemunerativosOtros, getSacSegundaCuotaOtrosEmpleos(liquidacion, db))
+	arrayRemunerativosOtros = append(arrayRemunerativosOtros, getHorasExtrasGravadasOtrosEmpleos(liquidacion, db))
+
+	totalRemunerativosOtros = Sum(arrayRemunerativosOtros)
+	return totalRemunerativosOtros
+}
+
+type strEscalaimpuestoaplicable struct {
+	Limiteinferior float64 `json:"limiteinferior"`
+	Limitesuperior float64 `json:"limitesuperior"`
+	Valorfijo      float64 `json:"valorfijo"`
+	Valorvariable  float64 `json:"valorvariable"`
+	Mesanio        string  `json:"mesanio"`
+}
+
+func getEscalaImpuestoAplicable(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) *[]strEscalaimpuestoaplicable {
+	var strescalaimpuestoaplicable []strEscalaimpuestoaplicable
+
+	anioLiquidacion := liquidacion.Fechaperiodoliquidacion.Year()
+	mesLiquidacion := getMes(&liquidacion.Fechaperiodoliquidacion)
+
+	mesAnioLiquidacion := strconv.Itoa(mesLiquidacion) + "/" + strconv.Itoa(anioLiquidacion)
+
+	sql := "SELECT limiteinferior,limitesuperior,valorfijo,valorvariable,mesanio FROM escalaimpuestoaplicable where mesanio = '" + mesAnioLiquidacion + "'"
+	db.Raw(sql).Scan(&strescalaimpuestoaplicable)
+
+	return &strescalaimpuestoaplicable
+
+}
+
+func getDeterminacionImpuestoFijo(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	var importeTotal float64 = 0
+	strescalaimpuestoaplicable := *getEscalaImpuestoAplicable(liquidacion, db)
+	totalganancianeta := getTotalGananciaNetaImponibleAcumuladaSinHorasExtras(liquidacion, db)
+
+	for i := 0; i < len(strescalaimpuestoaplicable); i++ {
+		escalaimpuestoaplicable := strescalaimpuestoaplicable[i]
+		if totalganancianeta > escalaimpuestoaplicable.Limiteinferior && totalganancianeta <= escalaimpuestoaplicable.Limitesuperior {
+			importeTotal = escalaimpuestoaplicable.Valorfijo
+		}
+	}
+	return importeTotal
+}
+
+func getDeterminacionImpuestoPorEscala(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	var importeTotal float64 = 0
+	strescalaimpuestoaplicable := *getEscalaImpuestoAplicable(liquidacion, db)
+	totalganancianeta := getTotalGananciaNetaImponibleAcumuladaSinHorasExtras(liquidacion, db)
+	baseimponible := getBaseImponible(liquidacion, db)
+	for i := 0; i < len(strescalaimpuestoaplicable); i++ {
+		escalaimpuestoaplicable := strescalaimpuestoaplicable[i]
+		if totalganancianeta > escalaimpuestoaplicable.Limiteinferior && totalganancianeta <= escalaimpuestoaplicable.Limitesuperior {
+
+			importeTotal = (baseimponible - escalaimpuestoaplicable.Limiteinferior) * escalaimpuestoaplicable.Valorvariable
+		}
+	}
+	return importeTotal
+}
+
+func getTotalRetener(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	var arrayTotalRetener []float64
+	var totalRetener float64
+
+	arrayTotalRetener = append(arrayTotalRetener, getDeterminacionImpuestoFijo(liquidacion, db))
+	arrayTotalRetener = append(arrayTotalRetener, getDeterminacionImpuestoPorEscala(liquidacion, db))
+
+	totalRetener = Sum(arrayTotalRetener)
+	return totalRetener
+}
+
+func getRetencionAcumulada(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	anioperiodoliquidacion := liquidacion.Fechaperiodoliquidacion.Year()
+	var totalconceptosimpuestoganancias float64
+	sql := "SELECT SUM(li.importeunitario) FROM siradig s INNER JOIN legajo le ON le.id = s.legajoid INNER JOIN liquidacion l ON le.id = l.legajoid INNER JOIN liquidacionitem li ON l.id = li.liquidacionid INNER JOIN  concepto c ON c.id = li.conceptoid WHERE c.codigo = 'IMPUESTO_GANANCIAS' AND to_char(s.periodosiradig, 'YYYY') = ' " + strconv.Itoa(anioperiodoliquidacion)
+	db.Raw(sql).Row().Scan(&totalconceptosimpuestoganancias)
+
+	return totalconceptosimpuestoganancias
+}
+
+func getRetencionMes(liquidacion *structLiquidacion.Liquidacion, db *gorm.DB) float64 {
+	totalRetener := getTotalRetener(liquidacion, db)
+	retencionAcumulada := getRetencionAcumulada(liquidacion, db)
+
+	return totalRetener - retencionAcumulada
+}
+
+func getDetalleCargoFamiliar(liquidacion *structLiquidacion.Liquidacion, columnaDetalleCargoFamiliar string, valorfijocolumna string, porcentaje float64, db *gorm.DB) float64 {
+	var importeTotal float64
+	var tienevalorbeneficio bool
 	anioperiodoliquidacion := liquidacion.Fechaperiodoliquidacion.Year()
 	mesperiodoliquidacion := getMes(&liquidacion.Fechaperiodoliquidacion)
 
@@ -302,29 +505,29 @@ func getDetalleCargoFamiliar(liquidacion *structLiquidacion.Liquidacion, columna
 	sql := "SELECT dcfs.* FROM siradig s INNER JOIN detallecargofamiliarsiradig dcfs ON s.id = dcfs.siradigid where to_char(periodosiradig, 'YYYY') = ' " + strconv.Itoa(anioperiodoliquidacion) + "' AND dcfs." + columnaDetalleCargoFamiliar + " NOTNULL AND s.legajoid = " + strconv.Itoa(*liquidacion.Legajoid)
 	db.Raw(sql).Scan(&detallecargofamiliar)
 
-	/*sql = "SELECT valor FROM siradig s INNER JOIN beneficiosiradig bs ON s.id = bs.siradigid WHERE " + mesperiodoliquidacion + " BETWEEN bs.mesdesde AND bs.meshasta AND to_char(periodosiradig, 'YYYY') = ' " + strconv.Itoa(anioperiodoliquidacion) + "'"
-	db.Raw(sql).Row().Scan(&valorbeneficio)
-	*/
+	sql = "SELECT valor FROM siradig s INNER JOIN beneficiosiradig bs ON s.id = bs.siradigid WHERE '" + strconv.Itoa(mesperiodoliquidacion) + "' > to_char(bs.mesdesde, 'MM') AND '" + strconv.Itoa(mesperiodoliquidacion) + "' < to_char(bs.meshasta, 'MM') AND to_char(s.periodosiradig, 'YYYY') = ' " + strconv.Itoa(anioperiodoliquidacion) + "'"
+	db.Raw(sql).Row().Scan(&tienevalorbeneficio)
+
 	if detallecargofamiliar.ID != 0 {
 
 		mesdadobaja := getMes(detallecargofamiliar.Meshasta)
 		mesdadoalta := getMes(detallecargofamiliar.Mesdesde)
-		valorfijoconyuge := getValorFijoImpuestoGanancia(liquidacion, "deduccionespersonales", "valorfijoconyuge", db)
+		valorfijo := getValorFijoImpuestoGanancia(liquidacion, "deduccionespersonales", valorfijocolumna, db)
 
-		/*if valorbeneficio != false {
-			valorfijoconyuge = valorfijoconyuge * 1.22
-		}*/
+		if tienevalorbeneficio == false {
+			valorfijo = valorfijo * 1.22
+		}
 
 		if mesdadobaja == 0 {
-			importeTotal = (valorfijoconyuge / 12) * float64(mesperiodoliquidacion-mesdadoalta)
+			importeTotal = (valorfijo / 12) * float64(mesperiodoliquidacion-mesdadoalta) * porcentaje
 		}
 
 		if mesdadobaja <= mesperiodoliquidacion {
-			importeTotal = (valorfijoconyuge / 12) * float64(mesdadobaja-mesdadoalta)
+			importeTotal = (valorfijo / 12) * float64(mesdadobaja-mesdadoalta) * porcentaje
 		}
 
 		if mesdadobaja > mesperiodoliquidacion {
-			importeTotal = (valorfijoconyuge / 12) * float64(mesperiodoliquidacion-mesdadoalta)
+			importeTotal = (valorfijo / 12) * float64(mesperiodoliquidacion-mesdadoalta) * porcentaje
 		}
 	}
 
