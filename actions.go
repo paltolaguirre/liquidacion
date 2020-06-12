a48e0540959caf788bfa51f0512cde0df5b49cad
Walter <wschmidt@xubio.com>
2020-06-12 14:42:49 -0300

    [feature/wschmidt-svc71] Ahora el duplicar elemina las novedades

--- actions.go
+++ actions.go
@@ -763,6 +763,7 @@ func LiquidacionDuplicarMasivo(w http.ResponseWriter, r *http.Request) {
 		for index := 0; index < len(duplicarLiquidacionesData.Idstoreplicate); index++ {
 			var liquidacionID = duplicarLiquidacionesData.Idstoreplicate[index]
 			var liquidacion structLiquidacion.Liquidacion
+			var liquidacionTipo structLiquidacion.Liquidaciontipo
 			var procesamientoStatus ProcesamientoStatus
 
 			//gorm:auto_preload se usa para que complete todos los struct con su informacion
@@ -776,13 +777,14 @@ func LiquidacionDuplicarMasivo(w http.ResponseWriter, r *http.Request) {
 				/* se modifica liquidacion a duplicar */
 				liquidacion.ID = 0
 				liquidacion.Tipoid = duplicarLiquidacionesData.Liquidaciondefaultvalues.Tipoid
-				if err := db.Set("gorm:auto_preload", true).First(&liquidacion.Tipo, "id = ?", liquidacion.Tipoid).Error; gorm.IsRecordNotFoundError(err) {
+				if err := db.Set("gorm:auto_preload", true).First(&liquidacionTipo, "id = ?", *liquidacion.Tipoid).Error; gorm.IsRecordNotFoundError(err) {
 					procesamientoStatus.Id = *liquidacion.Tipoid
 					procesamientoStatus.Tipo = "ERROR"
 					procesamientoStatus.Codigo = http.StatusNotFound
 					procesamientoStatus.Mensaje = err.Error()
 					procesamientoMasivo.Result = append(procesamientoMasivo.Result, procesamientoStatus)
 				}
+				liquidacion.Tipo = &liquidacionTipo
 				liquidacion.Fecha = duplicarLiquidacionesData.Liquidaciondefaultvalues.Fecha
 				liquidacion.Fechaultimodepositoaportejubilatorio = duplicarLiquidacionesData.Liquidaciondefaultvalues.Fechaultimodepositoaportejubilatorio
 				liquidacion.Fechaperiododepositado = duplicarLiquidacionesData.Liquidaciondefaultvalues.Fechaperiododepositado
@@ -801,59 +803,31 @@ func LiquidacionDuplicarMasivo(w http.ResponseWriter, r *http.Request) {
 					liquidacion.Situacionrevistatres = duplicarLiquidacionesData.Liquidaciondefaultvalues.Situacionrevistatres
 					liquidacion.Fechasituacionrevistatres = &duplicarLiquidacionesData.Liquidaciondefaultvalues.Fechaperiodoliquidacion
 				}
+
+				var liquidacionItems[] structLiquidacion.Liquidacionitem
 				for index := 0; index < len(liquidacion.Liquidacionitems); index++ {
-					liquidacion.Liquidacionitems[index].ID = 0
-					liquidacion.Liquidacionitems[index].CreatedAt = time.Time{}
-					liquidacion.Liquidacionitems[index].UpdatedAt = time.Time{}
-					liquidacion.Liquidacionitems[index].Liquidacionid = 0
-					liquidacion.Liquidacionitems[index].Acumuladores = nil
-					if !liquidacion.Liquidacionitems[index].Concepto.Eseditable {
-						recalcularLiquidacionItem(&liquidacion.Liquidacionitems[index], liquidacion, db, r.Header.Get("Authorization"))
+					var concepto structConcepto.Concepto
+					if err := db.Set("gorm:auto_preload", true).First(&concepto, "id = ?", *liquidacion.Liquidacionitems[index].Conceptoid).Error; gorm.IsRecordNotFoundError(err) {
+						procesamientoStatus.Id = *liquidacion.Liquidacionitems[index].Conceptoid
+						procesamientoStatus.Tipo = "ERROR"
+						procesamientoStatus.Codigo = http.StatusNotFound
+						procesamientoStatus.Mensaje = err.Error()
+						procesamientoMasivo.Result = append(procesamientoMasivo.Result, procesamientoStatus)
 					}
-
+					if !concepto.Esnovedad {
+						liquidacion.Liquidacionitems[index].ID = 0
+						liquidacion.Liquidacionitems[index].CreatedAt = time.Time{}
+						liquidacion.Liquidacionitems[index].UpdatedAt = time.Time{}
+						liquidacion.Liquidacionitems[index].Liquidacionid = 0
+						liquidacion.Liquidacionitems[index].Acumuladores = nil
+						if !liquidacion.Liquidacionitems[index].Concepto.Eseditable {
+							recalcularLiquidacionItem(&liquidacion.Liquidacionitems[index], liquidacion, db, r.Header.Get("Authorization"))
+						}
+						liquidacionItems = append(liquidacionItems, liquidacion.Liquidacionitems[index])
+					}
 				}
+				liquidacion.Liquidacionitems = liquidacionItems
 
-				/*for index := 0; index < len(liquidacion.Importesremunerativos); index++ {
-					liquidacion.Importesremunerativos[index].ID = 0
-					liquidacion.Importesremunerativos[index].CreatedAt = time.Time{}
-					liquidacion.Importesremunerativos[index].UpdatedAt = time.Time{}
-					liquidacion.Importesremunerativos[index].Liquidacionid = 0
-				}
-				for index := 0; index < len(liquidacion.Importesnoremunerativos); index++ {
-					liquidacion.Importesnoremunerativos[index].ID = 0
-					liquidacion.Importesnoremunerativos[index].CreatedAt = time.Time{}
-					liquidacion.Importesnoremunerativos[index].UpdatedAt = time.Time{}
-					liquidacion.Importesnoremunerativos[index].Liquidacionid = 0
-				}
-				for index := 0; index < len(liquidacion.Descuentos); index++ {
-					liquidacion.Descuentos[index].ID = 0
-					liquidacion.Descuentos[index].CreatedAt = time.Time{}
-					liquidacion.Descuentos[index].UpdatedAt = time.Time{}
-					liquidacion.Descuentos[index].Liquidacionid = 0
-				}
-				for index := 0; index < len(liquidacion.Retenciones); index++ {
-					liquidacion.Retenciones[index].ID = 0
-					liquidacion.Retenciones[index].CreatedAt = time.Time{}
-					liquidacion.Retenciones[index].UpdatedAt = time.Time{}
-					liquidacion.Retenciones[index].Liquidacionid = 0
-				}
-				for index := 0; index < len(liquidacion.Aportespatronales); index++ {
-					liquidacion.Aportespatronales[index].ID = 0
-					liquidacion.Aportespatronales[index].CreatedAt = time.Time{}
-					liquidacion.Aportespatronales[index].UpdatedAt = time.Time{}
-					liquidacion.Aportespatronales[index].Liquidacionid = 0
-				}*/
-
-				/*liquidacionJSON, _ := json.Marshal(liquidacion)
-				fmt.Println(string(liquidacionJSON))*/
-
-				/*decoder2 := json.NewDecoder(strings.NewReader(string(liquidacionJSON)))
-
-				var liquidacion2 structLiquidacion.Liquidacion
-				if err := decoder2.Decode(&liquidacion2); err != nil {
-					framework.RespondError(w, http.StatusBadRequest, err.Error())
-					return
-				}*/
 
 				if err := db.Create(&liquidacion).Error; err != nil {
 					procesamientoStatus.Id = liquidacionID
